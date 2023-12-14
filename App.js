4a25ab4e4e3a8818b442b9b04263ac674d5f27f4
techmax <techmax@inbox.ru>
2023-12-14 20:42:22 +0300

    three commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!dfgdf</Text>
+      <Text>Open up App.js to start working on your app!dfgdf1</Text>
       <StatusBar style="auto" />
     </View>
   );
